cb27f379170f34053993cac53c332f42e2d2bcf7
Trevor Brown <tbrown@voalte.com>
2014-11-18 17:58:25 -0500

    Updated the pattern matching code samples.

--- code_samples/pattern_matching.ex
+++ code_samples/pattern_matching.ex
@@ -1,12 +1,21 @@
-# In Elixir, just like in Erlang, the `=` denotes pattern matching
-:a = :a
-#=> :a
-:a = :b
-#=> ** (MatchError) no match of right hand side value: :b
+# First slide
+:a = :a #=> :a
+:a = :b #=> ** (MatchError) no match of right hand side value: :b
+letter = :a #=> :a
+letter #=> :a
 
+# Second slide
 # Atoms can be used in pattern matching
 {:ok, foo} = {:ok, "foo bar baz"}
 foo #=> "foo bar baz"
+
+# Third slide
+{num, num} = {1, 1} #=> {1, 1}
+{num, num} = {1, 2} #=> ** (MatchError) no match of right hand side value: {1, 2}
+
+# Fourth slide
+{_, _} = {1, 1} #=> {1, 1}
+{_, _} = {1, 2} #=> {1, 2}
 
 {:ok, foo} = {:error, :crashed}
 #=> ** (MatchError) no match of right hand side value: {:error, :crashed}
