29b7321a8543ddacdaebec64792339ea67fb016a
Trevor Brown <tbrown@voalte.com>
2014-11-19 09:28:53 -0500

    More work on the pattern matching code samples.

--- code_samples/pattern_matching.ex
+++ code_samples/pattern_matching.ex
@@ -8,6 +8,8 @@ letter #=> :a
 # Atoms can be used in pattern matching
 {:ok, foo} = {:ok, "foo bar baz"}
 foo #=> "foo bar baz"
+{:ok, foo} = {:error, :crashed}
+#=> ** (MatchError) no match of right hand side value: {:error, :crashed}
 
 # Third slide
 {num, num} = {1, 1} #=> {1, 1}
@@ -16,15 +18,16 @@ foo #=> "foo bar baz"
 # Fourth slide
 {_, _} = {1, 1} #=> {1, 1}
 {_, _} = {1, 2} #=> {1, 2}
+# This fails
+{num, num} = {1, 2} #=> ** (MatchError) no match of right hand side value: {1, 2}
 
-{:ok, foo} = {:error, :crashed}
-#=> ** (MatchError) no match of right hand side value: {:error, :crashed}
-
+# Fifth slide
 # It's common practice to assign a value to `_` if we don't need it
 {status, _} = {:error, :crashed}
 status #=> :error
-
 # Patter matching a list
 [head|tail] = [1,2,3]
 head #=> 1
 tail #=> [2,3]
+
+# Sixth slide
