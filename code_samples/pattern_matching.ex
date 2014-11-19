84d616b281830d52290668e9799b521a5eb0ed81
Trevor Brown <tbrown@voalte.com>
2014-11-19 14:08:35 -0500

    Updated the pattern matching samples with more on matching lists.

--- code_samples/pattern_matching.ex
+++ code_samples/pattern_matching.ex
@@ -12,25 +12,34 @@ foo #=> "foo bar baz"
 #=> ** (MatchError) no match of right hand side value: {:error, :crashed}
 
 # Third slide
+# Patter matching a list
+[head|tail] = [1,2,3]
+head #=> 1
+tail #=> [2,3]
+[first,second,third] = [1,2,3]
+second #=> 2
+[one,two] = [1,2,3] #=> ** (MatchError) no match of right hand side value: [1, 2, 3]
+[first,second|rest] = [1,2,3]
+second #=> 2
+
+# Fourth slide
 {num, num} = {1, 1} #=> {1, 1}
 {num, num} = {1, 2} #=> ** (MatchError) no match of right hand side value: {1, 2}
 
-# Fourth slide
+# Fifth slide
 {_, _} = {1, 1} #=> {1, 1}
 {_, _} = {1, 2} #=> {1, 2}
 # This fails
 {num, num} = {1, 2} #=> ** (MatchError) no match of right hand side value: {1, 2}
 
-# Fifth slide
+# Sixth slide
 # It's common practice to assign a value to `_` if we don't need it
 {status, _} = {:error, :crashed}
 status #=> :error
-# Patter matching a list
-[head|tail] = [1,2,3]
-head #=> 1
-tail #=> [2,3]
+{status, _, _} = {:ok, [], []}
+status #=> :ok
 
-# Sixth slide
+# Seventh slide
 # These are not parameters, they are patterns
 # evaluated if value passed in matches `0`
 def fib(0) do 0 end
@@ -39,7 +48,7 @@ def fib(1) do 1 end
 # evaluated if the first two clauses don't match
 def fib(n) do fib(n-1) + fib(n-2) end
 
-# Seventh slide
+# Eighth slide
 case do_something(work) do
   {:ok, result} -> # success
     do_something_with_result(result)
