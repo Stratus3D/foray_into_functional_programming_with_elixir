6ca36a2a199a85ff5059c8f0645f9722c9788d16
Trevor Brown <tbrown@voalte.com>
2014-11-26 11:46:32 -0500

    More work on the recursion code samples.

--- code_samples/recursion.ex
+++ code_samples/recursion.ex
@@ -1,3 +1,4 @@
+# Third slide
 # Added all the items in the list together to compute the sum
 def sum_list([head | tail], acc) do
   sum_list(tail, acc + head)
@@ -7,13 +8,26 @@ def sum_list([], acc) do
   acc
 end
 
-def factoral(0) do
+# Fourth slide
+def factorial(0) do
   1
 end
-def factoral(number) do
+
+def factorial(number) do
   factoral(number - 1) * number
 end
 
+# Fifth slide
+# Tail call optimized version of factorial function
+def factorial(0, result) do
+  result
+end
+
+def factorial(number, result) do
+  factoral(number - 1, result * number)
+end
+
+# Misc
 # Implementation of reduce
 def reduce([], value, _) do
   value
